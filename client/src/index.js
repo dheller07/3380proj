7e36ae0fe2aa240e7274d604787f48f11f80a39b
Caitlin Dooley <ccdooley94@gmail.com>
2021-04-05 16:53:58 -0500

    updated

--- client/src/index.js
+++ client/src/index.js
@@ -1,23 +1,12 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from "react-router-dom";
-import './index.css';
 
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import Amplify from 'aws-amplify';
-import config from './aws-exports';
-Amplify.configure(config);
+
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-);
 
-serviceWorker.unregister();
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <App />,
+    document.getElementById('root')
+);
