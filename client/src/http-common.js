8d574c4fed223429f2ff2214e576bb207b3653fb
Caitlin Dooley <ccdooley94@gmail.com>
2021-04-25 17:56:00 -0500

    removed services

--- client/src/http-common.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-    // base url depends on server config
-    baseURL: "http://localhost:8080/api",
-    headers: {
-        "Content-type": "application/json"
-    }
-});
